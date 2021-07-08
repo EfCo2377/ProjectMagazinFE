ee15bf7260b372825cd8efe074a9a3b3c21dc3f2
EfCo2377 <efrat0548502377@gmail.com>
2021-07-08 20:31:56 +0300

    add-redux

---
+++ src/redux/actions.js
@@ -0,0 +1,18 @@
+function convertActionName(actionName){//addPupil
+    return actionName.replace(/([A-Z])/g,"_$1").toUpperCase()//ADD_PUPIL
+}
+
+// let x=actions.addPupil({name:'ddd',id:'ffff'})
+const actions = new Proxy(
+    {},
+    {
+        get: function (target, prop) {//prop='addPupil'
+            if (target[prop] === undefined)
+                return function (args) {//args={name:'ddd',id:'ffff'}
+                    return { type: convertActionName(prop), payload: args }
+                }
+            else return target[prop]
+        }
+    }
+)
+export default actions
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+
+const reducer=combineReducers({})
+const store=createStore(reducer,applyMiddleware())
+window.store=store
+export default store
